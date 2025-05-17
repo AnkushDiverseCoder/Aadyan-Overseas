3f9d7e6a1eca9ba6d525c1130e2a21f177d6ff0d
AnkushDiverseCoder <thakurankushsingh1902@gmail.com>
2025-05-17 23:34:25 +0530

    Website changes with courses

---
+++ assets/js/courseData.js
@@ -0,0 +1,314 @@
+const uk =  [
+        {
+            "title": "MSc Computing and Information Systems",
+            "university": "University of South Wales, UK",
+            "price": "£16,900",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MA Film (Production Management)",
+            "university": "University of South Wales, UK",
+            "price": "£16,900",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Electronics and Information Technology",
+            "university": "University of South Wales, UK",
+            "price": "£16,900",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "BSc (Hons) International Business and Management",
+            "university": "University of South Wales, UK",
+            "price": "£15,850",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Strategic and Digital Marketing",
+            "university": "University of South Wales, UK",
+            "price": "£16,900",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "LLM Legal Practice",
+            "university": "University of South Wales, UK",
+            "price": "£17,900",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "LLM Law, Technology and Innovation",
+            "university": "University of South Wales, UK",
+            "price": "£14,950",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MA Design Innovation",
+            "university": "University of South Wales, UK",
+            "price": "£16,900",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Public Health",
+            "university": "University of South Wales, UK",
+            "price": "£16,900",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MEng Mechanical Engineering",
+            "university": "University of South Wales, UK",
+            "price": "£16,200",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "LLM Professional Practice (SQE)",
+            "university": "University of South Wales, UK",
+            "price": "£16,900",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MA Film",
+            "university": "University of South Wales, UK",
+            "price": "£15,850",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "BSc (Hons) Nursing (Child)",
+            "university": "University of South Wales, UK",
+            "price": "£16,200",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Computational Engineering",
+            "university": "Swansea University, UK",
+            "price": "£21,200",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Computer Science",
+            "university": "Swansea University, UK",
+            "price": "£20,300",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Advanced Computer Science",
+            "university": "Swansea University, UK",
+            "price": "£20,900",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Computational Engineering with Industry",
+            "university": "Swansea University, UK",
+            "price": "£21,200",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Computer Science - Informatique (Swansea)",
+            "university": "Swansea University, UK",
+            "price": "£10,050",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "PGCert Secondary PGCE with QTS: Computer Science",
+            "university": "Swansea University, UK",
+            "price": "£17,500",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Data Science",
+            "university": "Swansea University, UK",
+            "price": "£20,900",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Management (International Management)",
+            "university": "Swansea University, UK",
+            "price": "£20,600",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Cyber Security",
+            "university": "Swansea University, UK",
+            "price": "£20,900",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Applied Data Science",
+            "university": "Swansea University, UK",
+            "price": "£19,100",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "Ph.D Business Management",
+            "university": "Swansea University, UK",
+            "price": "£18,500",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "BSc (Hons) Logistics with Supply Chain Management",
+            "university": "Aston University, UK",
+            "price": "£21,500",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "BSc (Hons) Psychology and Sociology",
+            "university": "Aston University, UK",
+            "price": "£21,500",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "BSc Computer Science (Software Engineering) with Placement Year",
+            "university": "Brunel University of London, UK",
+            "price": "£19,330",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "LLM International Financial Regulation and Corporate Law",
+            "university": "Brunel University of London, UK",
+            "price": "£19,330",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Advanced Engineering Design",
+            "university": "Brunel University of London, UK",
+            "price": "£23,615",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MSc Accounting and Business Intelligence",
+            "university": "Brunel University of London, UK",
+            "price": "£24,795",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        },
+        {
+            "title": "MBBS Medicine",
+            "university": "Brunel University of London, UK",
+            "price": "£49,395",
+            "category": "uk",
+            "image": {
+                "university_logo": "",
+                "instructor_avatar": ""
+            }
+        }
+    ]
+    
+export default uk

5369db3e78d80d0ce7c1f4494827d4a937a5865e
AnkushDiverseCoder <thakurtulja0@gmail.com>
2025-05-21 14:52:57 +0530

    University Page added

---
+++ assets/js/universityData.js
@@ -0,0 +1,442 @@
+export default [
+    {
+        "United States": [
+            {
+                "title": "Marist College",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/marist-college.png",
+                }
+            },
+            {
+                "title": "Roosevelt University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/roosevelt-university.png",
+                }
+            },
+            {
+                "title": "University at Buffalo (UB)",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-at-buffalo-ub.png",
+                }
+            },
+            {
+                "title": "University of New Hampshire (UNH)",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-new-hampshire-unh.png",
+                }
+            },
+            {
+                "title": "Wright State University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/wright-state-university.png",
+                }
+            },
+            {
+                "title": "Hult International Business School",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/hult-international-business-school.png",
+                }
+            },
+            {
+                "title": "Gannon University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/gannon-university.png",
+                }
+            },
+            {
+                "title": "University of Arizona",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-arizona.png",
+                }
+            },
+            {
+                "title": "Regis University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/regis-university.png",
+                }
+            },
+            {
+                "title": "Concordia University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/concordia-university.png",
+                }
+            },
+            {
+                "title": "Illinois Institute of Technology",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/illinois-institute-of-technology.png",
+                }
+            },
+            {
+                "title": "Lawrence Technological University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/lawrence-technological-university.png",
+                }
+            },
+            {
+                "title": "University of Bridgeport",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-bridgeport.png",
+                }
+            },
+            {
+                "title": "New York Institute of Technology",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/new-york-institute-of-technology.png",
+                }
+            },
+            {
+                "title": "New Jersey Institute of Technology",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/new-jersey-institute-of-technology.png",
+                }
+            },
+            {
+                "title": "Cleveland State University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/cleveland-state-university.png",
+                }
+            },
+            {
+                "title": "Webster University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/webster-university.png",
+                }
+            },
+            {
+                "title": "Findlay University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/findlay-university.png",
+                }
+            }
+        ],
+        "United Kingdom": [
+            {
+                "title": "University of Wolverhampton",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-wolverhampton.png",
+                }
+            },
+            {
+                "title": "Birmingham City University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/birmingham-city-university.png",
+                }
+            },
+            {
+                "title": "Heriot-Watt University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/heriot-watt-university.png",
+                }
+            },
+            {
+                "title": "Northumbria University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/northumbria-university.png",
+                }
+            },
+            {
+                "title": "University of the West of England (UWE Bristol)",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-the-west-of-england-uwe-bristol.png",
+                }
+            },
+            {
+                "title": "Coventry University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/coventry-university.png",
+                }
+            },
+            {
+                "title": "University of Central Lancashire (UCLan)",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-central-lancashire-uclan.png",
+                }
+            },
+            {
+                "title": "University of Bolton",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-bolton.png",
+                }
+            },
+            {
+                "title": "University of Gloucestershire",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-gloucestershire.png",
+                }
+            },
+            {
+                "title": "Wrexham Glyndŵr University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/wrexham-glyndŵr-university.png",
+                }
+            },
+            {
+                "title": "York St John University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/york-st-john-university.png",
+                }
+            },
+            {
+                "title": "University of Buckingham",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-buckingham.png",
+                }
+            },
+            {
+                "title": "University of East London",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-east-london.png",
+                }
+            },
+            {
+                "title": "University of Leicester",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-leicester.png",
+                }
+            },
+            {
+                "title": "Greenwich University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/greenwich-university.png",
+                }
+            },
+            {
+                "title": "Nottingham Trent University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/nottingham-trent-university.png",
+                }
+            },
+            {
+                "title": "University of Chester",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-chester.png",
+                }
+            },
+            {
+                "title": "University of Hertfordshire",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-hertfordshire.png",
+                }
+            },
+            {
+                "title": "University of Law",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-law.png",
+                }
+            },
+            {
+                "title": "University of Surrey",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-surrey.png",
+                }
+            },
+            {
+                "title": "De Montfort University (DMU)",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/de-montfort-university-dmu.png",
+                }
+            },
+            {
+                "title": "Edinburgh Napier University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/edinburgh-napier-university.png",
+                }
+            }
+        ],
+        "Canada": [
+            {
+                "title": "University of Toronto",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-toronto.png",
+                }
+            },
+            {
+                "title": "University of British Columbia (UBC)",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-british-columbia-ubc.png",
+                }
+            },
+            {
+                "title": "McGill University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/mcgill-university.png",
+                }
+            },
+            {
+                "title": "University of Alberta",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-alberta.png",
+                }
+            },
+            {
+                "title": "University of Waterloo",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-waterloo.png",
+                }
+            },
+            {
+                "title": "Simon Fraser University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/simon-fraser-university.png",
+                }
+            },
+            {
+                "title": "York University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/york-university.png",
+                }
+            },
+            {
+                "title": "University of Ottawa",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-ottawa.png",
+                }
+            },
+            {
+                "title": "Western University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/western-university.png",
+                }
+            },
+            {
+                "title": "Dalhousie University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/dalhousie-university.png",
+                }
+            }
+        ],
+        "Australia": [
+            {
+                "title": "University of Melbourne",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-melbourne.png",
+                }
+            },
+            {
+                "title": "Australian National University (ANU)",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/australian-national-university-anu.png",
+                }
+            },
+            {
+                "title": "University of Sydney",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-sydney.png",
+                }
+            },
+            {
+                "title": "University of Queensland (UQ)",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-queensland-uq.png",
+                }
+            },
+            {
+                "title": "University of New South Wales (UNSW)",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-new-south-wales-unsw.png",
+                }
+            },
+            {
+                "title": "Monash University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/monash-university.png",
+                }
+            },
+            {
+                "title": "University of Western Australia (UWA)",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-western-australia-uwa.png",
+                }
+            },
+            {
+                "title": "University of Adelaide",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/university-of-adelaide.png",
+                }
+            },
+            {
+                "title": "Macquarie University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/macquarie-university.png",
+                }
+            },
+            {
+                "title": "RMIT University",
+                "university": "Total Courses: 0",
+                "image": {
+                    "university_logo": "assets/images/universities/rmit-university.png",
+                }
+            }
+        ]
+    }
+];
+
+
+// For flag CDN URLs for the specified countries, you can use the following resources:
+
+// 1. **United States**: [https://flagcdn.com/w320/us.png](https://flagcdn.com/w320/us.png)
+// 2. **United Kingdom**: [https://flagcdn.com/w320/gb.png](https://flagcdn.com/w320/gb.png)
+// 3. **Canada**: [https://flagcdn.com/w320/ca.png](https://flagcdn.com/w320/ca.png)
+// 4. **Australia**: [https://flagcdn.com/w320/au.png](https://flagcdn.com/w320/au.png)
+
+// These links provide the flag images in PNG format.
